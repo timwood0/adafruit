d8ba5ad177c7c55afc740038eccd0065c14e6af2
Tim Wood <timwood0@pacbell.net>
2020-07-04 16:17:42 -0700

    Track this thing, finally.

---
+++ i2clight.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+# Vary an LED brightness up and down via a 4725 DAC
+
+import time
+import os
+import sys
+import Adafruit_MCP4725 as MCP4725
+
+# "Voltages" are unitless parts of 4096 multiplied by Vdd on-board the device
+lowV = 1792 # Approx. firing threshold of LED
+highV = 4095
+i2cAddr = 0x62
+mcp4725 = MCP4725.MCP4725(address=i2cAddr)
+
+outV = lowV
+period = 0.04 #sec
+delta = 100
+while True:
+	mcp4725.set_voltage(outV)
+	time.sleep(period)
+
+	if delta > 0:
+		if outV >= highV:
+			delta = -delta
+	else:
+		if outV <= lowV:
+			delta = -delta
+
+	outV += delta
